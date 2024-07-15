b3b2d4ea935d215596808e334261750d16a207d8
ChronicStone <thao@agorastore.fr>
2024-07-15 14:15:08 +0200

    test(lib): add basic tests

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,7 +1,28 @@
 import { describe, expect, it } from 'vitest'
+import { query } from '../src'
+import { generateUsers } from './fixtures/user.fixture'
 
-describe('should', () => {
-  it('exported', () => {
-    expect(1).toEqual(1)
+describe('should filter users', () => {
+  const users = generateUsers(100)
+  const activeUsers = users.filter(user => user.status === 'active')
+  it('should filter active users', () => {
+    const { totalPages, totalRows } = query(users, {
+      limit: 10,
+      page: 1,
+      sort: {
+        key: 'createdAt',
+        dir: 'desc',
+      },
+      filter: [
+        {
+          key: 'status',
+          matchMode: 'equals',
+          value: 'active',
+        },
+      ],
+    })
+
+    expect(totalRows).toEqual(activeUsers.length)
+    expect(totalPages).toEqual(Math.ceil(activeUsers.length / 10))
   })
 })
