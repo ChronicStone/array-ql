a574394b690e5569d3c7b86ed01cc9dd684f75fe
ChronicStone <thao@agorastore.fr>
2024-07-17 16:57:56 +0200

    refactor(tests): properly configure test runner from fixtures

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,27 +1,62 @@
 import { describe, expect, it } from 'vitest'
 import { query } from '../src'
-import { generateUsers } from './fixtures/user.fixture'
+import PaginationFixtures from './fixtures/pagination.fixture.json'
+import SortingFixtures from './fixtures/sorting.fixture.json'
+import FilteringFixtures from './fixtures/filtering.fixture.json'
+import SearchFixtures from './fixtures/search.fixture.json'
 
-describe('should filter users', () => {
-  const users = generateUsers(100)
-  const activeUsers = users.filter(user => user.status === 'active')
-  it('should filter active users', () => {
-    const { totalPages, totalRows } = query(users, {
+const fixtures = [
+  {
+    key: 'pagination',
+    tests: PaginationFixtures,
+  },
+  {
+    key: 'sorting',
+    tests: SortingFixtures,
+  },
+  {
+    key: 'search',
+    tests: SearchFixtures,
+  },
+  {
+    key: 'filtering',
+    tests: FilteringFixtures,
+  },
+]
+
+for (const fixture of fixtures) {
+  describe(`${fixture.key} tests`, () => {
+    for (const test of fixture.tests) {
+      it(test.title, () => {
+        const { unpaginatedRows, ...result } = (query as any)(test.data, test.query)
+        expect(result).toEqual(test.result)
+      })
+    }
+  })
+}
+
+describe('performance check', () => {
+  // VAL BETWEEN 1 & 100
+  const getValue = () => Math.floor(Math.random() * 100)
+  const items = Array.from({ length: 1000000 }, (_, i) => ({ id: i, name: `Item ${i}`, value: getValue() }))
+  it('query 1M rows - paginate + sort + search + filter in less than 500ms', () => {
+    const start = performance.now()
+    query(items, {
       limit: 10,
       sort: {
-        key: 'createdAt',
-        dir: 'desc',
+        key: 'id',
+        dir: 'asc',
       },
-      filter: [
-        {
-          key: 'status',
-          matchMode: 'equals',
-          value: 'active',
-        },
-      ],
+      search: {
+        value: 'Item 1',
+        keys: ['name'],
+      },
+      filter: [{ key: 'value', matchMode: 'greaterThan', value: 50 }],
     })
 
-    expect(totalRows).toEqual(activeUsers.length)
-    expect(totalPages).toEqual(Math.ceil(activeUsers.length / 10))
+    const end = performance.now()
+    // eslint-disable-next-line no-console
+    console.info('Time taken:', end - start)
+    expect(end - start).toBeLessThan(500)
   })
 })
